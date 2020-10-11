70a99587fbf44175eb251247d6c74f9c81bfa957
bshukaylo <bshukaylo@gmail.com>
2020-10-11 16:43:59 +0600

    added body-parser

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,11 @@
 module.exports.login = function (req, resp) {
-    resp.status(200).json({login: 'from controller'})
+    resp.status(200).json(
+        {
+            login: {
+                email: req.body.email,
+                password: req.body.password,
+            }
+        })
 }
 
 module.exports.register = function (req, resp) {
