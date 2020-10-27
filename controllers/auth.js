e2d1053f54404256d73ec3fa69c0e041af855380
bshukaylo <bshukaylo@gmail.com>
2020-10-27 22:25:15 +0600

    added jwt library. realized login method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,30 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const keys = require('../config/keys')
 
-module.exports.login = function (req, resp) {
-    resp.status(200).json(
-        {
-            login: {
-                email: req.body.email,
-                password: req.body.password,
+module.exports.login = async function (req, res) {
+    const candidate = await User.findOne({email: req.body.email})
+    if (candidate) {
+        const passwordsEqual = bcrypt.compareSync(req.body.password, candidate.password)
+        if (passwordsEqual) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60})
+            res.status(200).json({token: `Bearer ${token}`})
+        } else {
+            res.status(401).json({
+                message: 'Пароль неверный. Попробуйте снова'
+            })
+        }
+    } else {
+        res.status(404).json(
+            {
+                message: 'Пользователь не найден'
             }
-        })
+        )
+    }
 }
 
 module.exports.register = async function (req, res) {
