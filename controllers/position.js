f6b2b8d5b2ee10cb8baa042b338e564d7ba917e1
bshukaylo <bshukaylo@gmail.com>
2020-10-29 22:23:54 +0600

    positions controller logic implemented

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,53 @@
-module.exports.getByCategoryId = function (req, resp) {
+const Position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
 
+module.exports.getByCategoryId = async function (req, resp) {
+    try {
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        })
+        resp.status(200).json(positions)
+    } catch (e) {
+        errorHandler(resp, e)
+    }
 }
 
-module.exports.create = function (req, resp) {
-
+module.exports.create = async function (req, resp) {
+    try {
+        const position = await new Position({
+            name: req.body.name,
+            cost: req.body.cost,
+            category: req.body.category,
+            user: req.user.id
+        }).save()
+        resp.status(201).json(position)
+    } catch (e) {
+        errorHandler(resp, e)
+    }
 }
 
-module.exports.remove = function (req, resp) {
-
+module.exports.remove = async function (req, resp) {
+    try {
+        await Position.remove({_id: req.params.id})
+        resp.status(200).json({message: 'Позиция была удалена'})
+    } catch (e) {
+        errorHandler(resp, e)
+    }
 }
 
-module.exports.update = function (req, resp) {
-
+module.exports.update = async function (req, resp) {
+    try {
+        //filter -> по какому полю ищем
+        // doc -> какие поля меняем, т.к. $set, то те что в body
+        //options - new -> вернуть обновленную сущность, а не до изменения
+        const position = Position.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {new:true}
+            )
+        resp.status(200).json(position)
+    } catch (e) {
+        errorHandler(resp, e)
+    }
 }
