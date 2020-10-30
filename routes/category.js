95afb6b59949eeaac386d27c973067cb128c3593
Boris Shukaylo <bshukaylo@gmail.com>
2020-10-30 19:41:24 +0600

    fixed typo

--- routes/category.js
+++ routes/category.js
@@ -3,7 +3,7 @@ const passport = require('passport')
 const controllers = require('../controllers/category')
 const router = express.Router()
 
-router.get('/', passport.authenticate('jwt', {sessin:false}), controllers.getAll)
+router.get('/', passport.authenticate('jwt', {session:false}), controllers.getAll)
 router.get('/:id ', controllers.getById)
 router.delete('/:id', controllers.remove)
 router.post('/', controllers.create)
