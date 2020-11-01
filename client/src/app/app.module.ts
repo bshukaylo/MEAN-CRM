f3ea2325502f676ea8c4f83454f97f9ad1af6f7d
Boris Shukaylo <bshukaylo@gmail.com>
2020-11-01 16:40:24 +0600

    small refactoring. created register-page.component. added layout to login and register pages. updated routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,17 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
