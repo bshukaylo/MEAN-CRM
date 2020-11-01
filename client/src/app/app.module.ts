6d59ebc98e803ce32d155cb63c6c698e3bef4ead
Boris Shukaylo <bshukaylo@gmail.com>
2020-11-01 16:14:22 +0600

    created login-page component. added routing to front-end

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
