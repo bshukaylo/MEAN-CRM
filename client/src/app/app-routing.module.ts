7e6385cdabcdec4b4e3c9db940fde19f1637d46e
Boris Shukaylo <bshukaylo@gmail.com>
2020-11-02 16:57:58 +0600

    implemented auth.guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {LoginPageComponent} from "./login-page/login-page.component";
 import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
 import {SiteLayoutComponent} from "./shared/layouts/site-layout/site-layout.component";
 import {RegisterPageComponent} from "./register-page/register-page.component";
+import {AuthGuard} from "./shared/classes/auth.guard";
 
 const routes: Routes = [{
   path: '',
@@ -15,6 +16,7 @@ const routes: Routes = [{
 }, {
   path: '',
   component: SiteLayoutComponent,
+  canActivate: [AuthGuard],
   children: []
 }];
 
